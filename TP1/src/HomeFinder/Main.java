a49d6e8fe203e6bc521d34e88ed3995376415a4e
thetisiboth <thetisiboth@gmail.com>
2019-10-16 22:47:05 +0200

    Beginnning of the GUI of the HomeFinder

--- TP1/src/HomeFinder/Main.java
+++ TP1/src/HomeFinder/Main.java
@@ -2,9 +2,9 @@ package HomeFinder;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.util.Iterator;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,8 +20,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		SliderTestFrame frame = new SliderTestFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 
 	}
 
@@ -37,11 +35,13 @@ class SliderTestFrame extends JFrame {
 	private ChangeListener listenerPrice;
 	private JTextField tfRooms;
 	private JTextField tfPrice;
-	JPanel mainPanel;
-	
+	JPanel centerPanel;
+	JPanel rightPanel;
+
 	public SliderTestFrame() {
 		setTitle("TP1 - Home Finder");
-
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 		// common listener for all sliders
 		listenerRoom = new ChangeListener() {
 			public void stateChanged(ChangeEvent event) {
@@ -50,38 +50,67 @@ class SliderTestFrame extends JFrame {
 				tfRooms.setText("" + source.getValue() + " <->" + source.getUpperValue());
 			}
 		};
-		
-		listenerPrice= new ChangeListener() {
+
+		listenerPrice = new ChangeListener() {
 			public void stateChanged(ChangeEvent event) {
 				// update text field when the slider value changes
 				RangeSlider source = (RangeSlider) event.getSource();
-				tfPrice.setText("" + source.getValue()+ " <->" + source.getUpperValue());
+				tfPrice.setText("" + source.getValue() + " <->" + source.getUpperValue());
 			}
 		};
 
-		mainPanel = new JPanel();
-		JPanel rightPanel = new JPanel();
-		rightPanel.setLayout(new GridLayout(2,2));
-		mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		mainPanel.setPreferredSize(new Dimension(500, 500));
-		add(mainPanel);
+		centerPanel = new JPanel();
+		centerPanel.setPreferredSize(new Dimension(400, 400));
 
 		tfRooms = new JTextField();
+		tfRooms.setColumns(2);
+		tfRooms.setPreferredSize(new Dimension(20, 20));
 		tfPrice = new JTextField();
 		RangeSlider roomSlider = new RangeSlider();
-		addSliderPrice(roomSlider,"Room Slider");
+		addSliderPrice(roomSlider, "Room Slider");
 		RangeSlider priceSlider = new RangeSlider();
-		addSliderRoom(priceSlider,"Price Slider");
-		rightPanel.add(roomSlider, BorderLayout.NORTH);
-		
-		rightPanel.add(tfRooms);
-		
-		rightPanel.add(priceSlider, BorderLayout.CENTER);
-		rightPanel.add(tfPrice);
-		mainPanel.add(rightPanel, BorderLayout.EAST);
+		addSliderRoom(priceSlider, "Price Slider");
 
+		rightPanel = new JPanel();
+		rightPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10, 0, 0, 0);
+		c.weightx = 0;
+		c.weighty = 0;
+		c.gridx = 0;
+		c.gridy = 0;
+		rightPanel.add(roomSlider, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10, 0, 0, 0); // Top padding
+		c.weightx = 0;
+		c.weighty = 0;
+		c.ipadx = 50;
+		c.gridx = 0;
+		c.gridy = 1;
+		rightPanel.add(tfPrice, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(30, 0, 0, 0); // Top padding
+		c.ipadx = 0;
+		c.gridx = 0;
+		c.gridy = 2;
+		rightPanel.add(priceSlider, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(10, 0, 0, 0); // Top padding
+		c.weightx = 0.1;
+		c.weighty = 0.1;
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 1;
+		c.gridheight = 10;
+		rightPanel.add(tfRooms, c);
+
+		getContentPane().add(rightPanel, BorderLayout.EAST);
+		getContentPane().add(centerPanel, BorderLayout.CENTER);
 		pack();
 		setVisible(true);
+
 	}
 
 	public void addSliderPrice(JSlider s, String description) {
@@ -89,14 +118,14 @@ class SliderTestFrame extends JFrame {
 		JPanel panel = new JPanel();
 		panel.add(s);
 		panel.add(new JLabel(description));
-		mainPanel.add(panel);
+		getContentPane().add(panel);
 	}
-	
+
 	public void addSliderRoom(JSlider s, String description) {
 		s.addChangeListener(listenerRoom);
 		JPanel panel = new JPanel();
 		panel.add(s);
 		panel.add(new JLabel(description));
-		mainPanel.add(panel);
+		getContentPane().add(panel);
 	}
 }
