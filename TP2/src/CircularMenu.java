4fc54ef53266740c746144a0cd004e9ec62d49f7
devosx <devosx@gricad-gitlab.univ-grenoble-alpes.fr>
2019-11-14 12:16:15 +0100

    correction of edge placement for the circle

--- TP2/src/CircularMenu.java
+++ TP2/src/CircularMenu.java
@@ -10,16 +10,21 @@ import javax.swing.JPanel;
 public class CircularMenu extends JPanel {
 
 	private ArrayList<MenuItem> list;
-	private int radius;
+	private int radius_text;
+	private int radius_border;
+	private int diameter_border;
 	private Point center;
 	private Color color;
 
 	private void init() {
 		list = new ArrayList<MenuItem>();
-		radius = 100;
+		radius_text = 100;
+		radius_border = (int) (radius_text * 1.5);
+		diameter_border = radius_border*2;
+		
 		center = new Point(0, 0);
 		color = new Color(150, 150, 150);
-		setSize(radius * 3, radius * 3);
+		setSize(diameter_border, diameter_border);
 	}
 
 	public CircularMenu(ArrayList<MenuItem> l) {
@@ -47,24 +52,24 @@ public class CircularMenu extends JPanel {
 			if (list.size() < 9) {
 				for (int i = list.size() - 1; i >= 0; i--) {
 					taille = list.get(i).getLength();
-					list.get(i).setBounds((int) (xCentre - taille / 2 + radius * Math.cos(i * angle - Math.PI / 2)),
-							(int) (yCentre - 40 / 2 + radius * Math.sin(i * angle - Math.PI / 2)), taille + 10,
+					list.get(i).setBounds((int) (xCentre - taille / 2 + radius_text * Math.cos(i * angle - Math.PI / 2)),
+							(int) (yCentre - 40 / 2 + radius_text * Math.sin(i * angle - Math.PI / 2)), taille + 10,
 							40);
 					add(list.get(i));
 				}
 			} else {
 				for (int i = 7; i >= 0; i--) {
 					taille = list.get(i).getLength();
-					list.get(i).setBounds((int) (xCentre - taille / 2 + radius * Math.cos(i * angle - Math.PI / 2)),
-							(int) (yCentre - 40 / 2 + radius * Math.sin(i * angle - Math.PI / 2)), taille + 10,
+					list.get(i).setBounds((int) (xCentre - taille / 2 + radius_text * Math.cos(i * angle - Math.PI / 2)),
+							(int) (yCentre - 40 / 2 + radius_text * Math.sin(i * angle - Math.PI / 2)), taille + 10,
 							40);
 					add(list.get(i));
 				}
 				int shift = 1;
 				for (int i = list.size() -1; i > 7; i--,shift+=2) {
 					taille = list.get(i).getLength();
-					list.get(i).setBounds((int) (xCentre - taille / 2 + radius * Math.cos(4 * angle - Math.PI / 2)),
-							(int) (yCentre - 40 / 2 + radius * Math.sin(4 * angle - Math.PI / 2)) + taille + (taille*shift) , taille + 10,
+					list.get(i).setBounds((int) (xCentre - taille / 2 + radius_text * Math.cos(4 * angle - Math.PI / 2)),
+							(int) (yCentre - 40 / 2 + radius_text * Math.sin(4 * angle - Math.PI / 2)) + taille + (taille*shift) , taille + 10,
 							40);
 					add(list.get(i));
 				}
@@ -84,15 +89,15 @@ public class CircularMenu extends JPanel {
 		Point s1,s2;
 		int startAngle = (int)(arcAngle/2);
 		
-//		g2d.drawLine(radius, radius, s1.x, s1.y);
+//		g2d.drawLine(radius_text, radius_text, s1.x, s1.y);
 		for (int i = 0; i < list.size(); i++) {
-			s1 = new Point((int)(radius*1.5+radius*1.5*Math.cos(startAngle+arcAngle*i)),(int)(300 - (radius*1.5+radius*1.5*Math.sin(startAngle+arcAngle*i))));
-			s2 = new Point((int)(radius*1.5+radius*1.5*Math.cos(startAngle+arcAngle*(i+1))),(int)(radius*1.5+radius*1.5*Math.sin(startAngle+arcAngle*(i+1))));
+			s1 = new Point((int)(radius_border+radius_border*Math.cos(Math.PI*(startAngle+arcAngle*i)/180)),(int)(300 - (radius_border+radius_border*Math.sin(Math.PI*(startAngle+arcAngle*i)/180))));
+			//s2 = new Point((int)(radius_border+radius_border*Math.cos(startAngle+arcAngle*(i+1))),(int)(radius_border+radius_border*Math.sin(startAngle+arcAngle*(i+1))));
 			g2d.setColor(Color.RED);
-			g2d.fillArc(0,0, radius*3, radius*3, (int)((startAngle+arcAngle*i)%360), (int)arcAngle);
+			g2d.fillArc(0,0, diameter_border, diameter_border, (int)((startAngle+arcAngle*i)%360), (int)arcAngle);
 			g2d.setColor(Color.BLACK);
-			g2d.drawLine((int)(radius*1.5), (int)(radius*1.5), s1.x, s1.y);
-//			g2d.drawLine((int)(radius*1.5), (int)(radius*1.5), s2.x, s2.y);
+			g2d.drawLine(radius_border, radius_border, s1.x+i, s1.y+i);
+//			g2d.drawLine(radius_border, radius_border, s2.x, s2.y);
 		}
 //		g2d.fillRect(0, 0, getWidth(), getHeight());
 	}
